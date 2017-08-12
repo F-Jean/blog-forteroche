07e547d8ca20bc8ad9683b3a396bf67e025f1916
Jean <jean.courtot@gmail.com>
2017-08-12 15:37:45 +0200

    problem controller

--- app/Controller.php
+++ app/Controller.php
@@ -7,12 +7,8 @@
 
 namespace Framework;
 
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Tools\Setup;
-use Manager\CartManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
@@ -21,39 +17,39 @@ use Symfony\Component\HttpKernel\Controller\ControllerResolver;
  * @package Framework
  */
 class Controller {
+
   /**
    * @var \Twig_Environment
    */
-  private $twig;
+    private $twig;
 
   /**
    * Controller constructor.
    */
-  public function __construct() {
-    $loader = new \Twig_Loader_Filesystem([__DIR__.'/../src/View']);
-    $this->twig = new \Twig_Environment($loader, array(
-      'cache' => false,
-    ));
-  }
+    public function __construct() {
+      $loader = new \Twig_Loader_Filesystem([__DIR__.'/../src/View']);
+      $this->twig = new \Twig_Environment($loader, array(
+        'cache' => false,
+      ));
 
-  $dbParams = array(
-      'driver'   => 'pdo_mysql',
-      'user'     => "root",
-      'password' => "3A30d0_3919&Ceé748E%",
-      'dbname'   => "blog-forteroche",
-      'charset'  => 'utf8',
-  );
-  $config = Setup::createAnnotationMetadataConfiguration([__DIR__."/../src/Entity"], false, __DIR__."/../web/cache");
-  $config->setAutoGenerateProxyClasses(true);
-  $this->doctrine = EntityManager::create($dbParams, $config);
+      $dbParams = array(
+        'driver'   => 'pdo_mysql',
+        'user'     => "root",
+        'password' => "3A30d0_3919&Ceé748E%",
+        'dbname'   => "blog-forteroche",
+        'charset'  => 'utf8',
+      );
+      $config = Setup::createAnnotationMetadataConfiguration([__DIR__."/../src/Entity"], false, __DIR__."/../web/cache");
+      $config->setAutoGenerateProxyClasses(true);
+      $this->doctrine = EntityManager::create($dbParams, $config);
 
-  /**
-   * @param $filename
-   * @param $data
-   * @return Response
-   */
-  protected function render($filename, $data = []) {
-    $template = $this->twig->load($filename);
-    return new Response($template->render($data));
-  }
+    /**
+     * @param $filename
+     * @param $data
+     * @return Response
+     */
+     protected function render($filename, $data = []) {
+      $template = $this->twig->load($filename);
+      return new Response($template->render($data));
+    }
 }
