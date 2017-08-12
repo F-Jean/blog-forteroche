c037847b2d2686162bb28bbc8a1e4025729000d7
Jean <jean.courtot@gmail.com>
2017-08-12 15:59:42 +0200

    controller ok

--- app/Controller.php
+++ app/Controller.php
@@ -7,8 +7,11 @@
 
 namespace Framework;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
@@ -17,39 +20,39 @@ use Symfony\Component\HttpKernel\Controller\ControllerResolver;
  * @package Framework
  */
 class Controller {
-
   /**
    * @var \Twig_Environment
    */
-    private $twig;
+  private $twig;
 
   /**
    * Controller constructor.
    */
-    public function __construct() {
-      $loader = new \Twig_Loader_Filesystem([__DIR__.'/../src/View']);
-      $this->twig = new \Twig_Environment($loader, array(
-        'cache' => false,
-      ));
+  public function __construct() {
+    $loader = new \Twig_Loader_Filesystem([__DIR__.'/../src/View']);
+    $this->twig = new \Twig_Environment($loader, array(
+      'cache' => false,
+    ));
 
-      $dbParams = array(
+    $dbParams = array(
         'driver'   => 'pdo_mysql',
         'user'     => "root",
         'password' => "3A30d0_3919&Ceé748E%",
         'dbname'   => "blog-forteroche",
         'charset'  => 'utf8',
-      );
-      $config = Setup::createAnnotationMetadataConfiguration([__DIR__."/../src/Entity"], false, __DIR__."/../web/cache");
-      $config->setAutoGenerateProxyClasses(true);
-      $this->doctrine = EntityManager::create($dbParams, $config);
+    );
+    $config = Setup::createAnnotationMetadataConfiguration([__DIR__."/../src/Entity"], false, __DIR__."/../web/cache");
+    $config->setAutoGenerateProxyClasses(true);
+    $this->doctrine = EntityManager::create($dbParams, $config);
+  }
 
-    /**
-     * @param $filename
-     * @param $data
-     * @return Response
-     */
-     protected function render($filename, $data = []) {
-      $template = $this->twig->load($filename);
-      return new Response($template->render($data));
-    }
+  /**
+   * @param $filename
+   * @param $data
+   * @return Response
+   */
+  protected function render($filename, $data = []) {
+    $template = $this->twig->load($filename);
+    return new Response($template->render($data));
+  }
 }
