02d1f8e540d5fde132c6699b09e5f9b291de1e41
Jean <jean.courtot@gmail.com>
2017-08-12 13:53:24 +0200

    Create global Controller + set Twig parameters & creation of View repository

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,8 +9,8 @@ namespace Controller;
 
 use Framework\Controller;
 
-class DefaultController {
+class DefaultController extends Controller {
   public function indexAction() {
-    return $this->render();
+    return $this->render("index.html.twig", ["prenom" => "jean"]);
   }
 }
